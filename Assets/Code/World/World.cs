20e554d0574fd7d9b283b238ee1a61270728f0d9
Harald <harald921@hotmail.com>
2018-06-25 23:48:28 +0200

    Further work with world terrain generation

--- Assets/Code/World/World.cs
+++ Assets/Code/World/World.cs
@@ -4,7 +4,58 @@ using UnityEngine;
 
 public class World : MonoBehaviour
 {
+    [SerializeField] int _worldSize = 128;
 
+    Dictionary<Vector2DInt, Tile> _tiles = new Dictionary<Vector2DInt, Tile>();
+
+    View _view;
+
+
+    void Awake()
+    {
+        _view = new View(gameObject);
+    }
+
+
+    public class View
+    {
+        readonly Material _material;
+
+        readonly int _vertexSize;
+        readonly int _vertexCount;
+
+        readonly int[] _triangles;
+        readonly Vector3[] _vertices;
+
+        readonly MeshFilter   _meshFilter;
+        readonly MeshRenderer _meshRenderer;
+
+
+        public View(GameObject inViewGO)
+        {
+            _material = (Material)Resources.Load("Material_World", typeof(Material));
+
+            _meshFilter   = inViewGO.AddComponent<MeshFilter>();
+            _meshRenderer = inViewGO.AddComponent<MeshRenderer>();
+
+            _meshFilter.mesh = GenerateMesh();
+        }
+
+
+        Mesh GenerateMesh()
+        {
+            Mesh newMesh = new Mesh();
+
+
+
+            return newMesh;
+        }
+    }
+}
+
+public class Tile
+{
+    
 }
 
 
